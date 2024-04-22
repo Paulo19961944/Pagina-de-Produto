59602484ea631f1ac964bcd4f9534fd41e8855ae
Paulo19961944 <paulo24041996@gmail.com>
2024-04-22 11:50:51 -0300

    Página de Produto - v1.0.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,20 @@
-const bodyElement = window.document.body
-abrirMenuMobile.addEventListener('click', abrirMenu)
-fecharMenuMobile.addEventListener('click', fecharMenu)
+const bodyElement = window.document.body // Captura o Elemento Body
+abrirMenuMobile.addEventListener('click', abrirMenu) // Adiciona evento no Botão de Abrir Menu
+fecharMenuMobile.addEventListener('click', fecharMenu) // Adiciona evento no Botão de Fechar Menu
 
+//Função para Abrir o Menu
 function abrirMenu(){
-    bodyElement.style.overflow = 'hidden'
-    abrirMenuMobile.style.display = 'none'
-    fecharMenuMobile.style.display = 'flex'
-    menu.style.display = 'flex'
-    menu.style.flexDirection = 'column'
-    menu.style.alignItems = 'center'
-    menu.style.justifyContent = 'center'
-    menu.style.gap = '48px'
+    bodyElement.style.overflow = 'hidden' // Trava o elemento do Body em 100%
+    abrirMenuMobile.style.display = 'none' // Esconde o Botão de Abrir Menu
+    fecharMenuMobile.style.display = 'flex' // Esconde o Botão de Fechar Menu
+    menu.style.display = 'flex' // Flexbox
+    menu.style.flexDirection = 'column' // Alinha em Colunas
+    menu.style.alignItems = 'center' // Alinha o Eixo Perpendicular ao Centro
+    menu.style.justifyContent = 'center' // Alinha o Eixo Principal ao Centro
+    menu.style.gap = '48px' // Espaçamento entre os Itens
 }
 
+// Função de Fechar Menu
 function fecharMenu(){
-    window.location.reload()
+    window.location.reload() // Recarrega a Página
 }
