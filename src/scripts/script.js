ee823a1536a95152da1d309c10b3ed52cf3ae0de
Paulo19961944 <paulo24041996@gmail.com>
2024-04-22 12:59:03 -0300

    Página de Produto - v1.0.1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,17 +1,16 @@
 const bodyElement = window.document.body // Captura o Elemento Body
+const abrirMenuMobile = document.getElementById('abrirMenuMobile')
+const fecharMenuMobile = document.getElementById('fecharMenuMobile')
+const menu = document.getElementById('menu')
 abrirMenuMobile.addEventListener('click', abrirMenu) // Adiciona evento no Botão de Abrir Menu
 fecharMenuMobile.addEventListener('click', fecharMenu) // Adiciona evento no Botão de Fechar Menu
 
 //Função para Abrir o Menu
 function abrirMenu(){
-    bodyElement.style.overflow = 'hidden' // Trava o elemento do Body em 100%
-    abrirMenuMobile.style.display = 'none' // Esconde o Botão de Abrir Menu
-    fecharMenuMobile.style.display = 'flex' // Esconde o Botão de Fechar Menu
-    menu.style.display = 'flex' // Flexbox
-    menu.style.flexDirection = 'column' // Alinha em Colunas
-    menu.style.alignItems = 'center' // Alinha o Eixo Perpendicular ao Centro
-    menu.style.justifyContent = 'center' // Alinha o Eixo Principal ao Centro
-    menu.style.gap = '48px' // Espaçamento entre os Itens
+    bodyElement.classList.add('responsive-body') // Trava o elemento do Body em 100%
+    abrirMenuMobile.classList.add('abrir-responsive') // Esconde o Botão de Abrir Menu
+    fecharMenuMobile.classList.add('fechar-responsive') // Esconde o Botão de Fechar Menu
+    menu.classList.add('menu-responsive') // Aparece o Menu
 }
 
 // Função de Fechar Menu
